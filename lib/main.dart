accbac6d206e673a3598f744a05666d43f4f4612
Kookutility <gusdn555@gmail.com>
2023-11-23 18:57:58 +0900

    11/23 상단바, 알림스크린 추가 및 기본 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:petdemo/auth_screens/login_screen.dart';
-import 'package:petdemo/auth_screens/sigup_screen.dart';
-import 'package:petdemo/auth_screens/splash_screen.dart';
-import 'package:petdemo/const/address.dart';
+import 'package:petdemo/common/app_bar.dart';
+import 'package:petdemo/main_screens/chat_screen.dart';
+import 'package:petdemo/main_screens/common/bottom_nav_design.dart';
+import 'package:petdemo/main_screens/my_screen.dart';
+import 'package:petdemo/main_screens/search_screen.dart';
+import 'package:petdemo/main_screens/notification_screen.dart';
 
 void main() {
   runApp(const IsHere());
 }
 
 class IsHere extends StatelessWidget {
-  const IsHere({super.key});
+  const IsHere({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'isHere',
       debugShowCheckedModeBanner: false,
-      initialRoute: splashScreen,
-      routes: {
-        splashScreen: (context) => const SplashScreen(),
-        loginScreen: (context) => const LogInScreen(),
-        signUpScreen: (context) => const SignUpScreen(),
-      },
+      home: const BottomBar(), // Scaffold의 home을 BottomBar로 변경
+    );
+  }
+}
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  List<Widget> pages = const [
+    SearchScreen(),
+    ChatScreen(),
+    MyScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: IsHereAppBar(
+        onNotificationPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => NotificationScreen(), // main_screens/notification_screen.dart로 이동
+            ),
+          );
+        },
+        onSearchPressed: () {
+          // 검색 아이콘이 눌렸을 때의 동작
+        },
+      ),
+      bottomNavigationBar: BottomNavDesign(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: pages[selectedIndex],
     );
   }
 }
