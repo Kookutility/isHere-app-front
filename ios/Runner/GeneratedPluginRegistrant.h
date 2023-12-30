d71d442d8fbfea41480511859ef75b51bd6a0744
Byeongju Park <badmuxi@gmail.com>
2023-12-30 17:46:47 +0900

    feat: main 커밋 반영

--- ios/Runner/GeneratedPluginRegistrant.h
+++ ios/Runner/GeneratedPluginRegistrant.h
@@ -9,11 +9,11 @@
 
 #import <Flutter/Flutter.h>
 
-NS_ASSUME_NONNULL_BEGIN;
+NS_ASSUME_NONNULL_BEGIN
 
 @interface GeneratedPluginRegistrant : NSObject
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry;
 @end
 
-NS_ASSUME_NONNULL_END;
+NS_ASSUME_NONNULL_END
 #endif /* GeneratedPluginRegistrant_h */
